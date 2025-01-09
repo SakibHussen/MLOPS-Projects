0214ee3c81bc9fe62c33377a9cc9228ffd1bfb00
SakibHussen <sakibhussen7@gmail.com>
2025-01-08 23:32:28 -0500

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sakib Hussen",
+    author_email="sakibhussen7@gmail.com",
+    packages=find_packages()
+) 
